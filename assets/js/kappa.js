b88ba8487a673552c49ee5cd027c6fb9af065766
ddansa <anders@hvid.se>
2015-10-15 16:15:34 +0200

    Discover page WIP

--- assets/js/kappa.js
+++ assets/js/kappa.js
@@ -1,34 +1,41 @@
 
 //
 var api_url = "http://api.themoviedb.org/3/",
-	api_key = "?api_key=34a209538e9c2bf4cbcdef696f2bf6a2",
-	search_q;
+	api_key = "?api_key=34a209538e9c2bf4cbcdef696f2bf6a2";
+
+function apiURL(call, page) {
+	var txt = api_url + call + api_key;
+	return ($.isNumeric(page) && page <= 50) ?
+		txt + '&page=' + page :
+		txt;
+}
 
 $(function () {
 	$.ajax({
-			url: api_url + 'movie/popular' + api_key + '&page=1',
+			//url: api_url + 'movie/popular' + api_key + '&page=1',
+			url: apiURL('movie/popular', 1),
 			jsonp: 'callback',
 			dataType: 'jsonp',
 		}).success(function (data){
 			$.each(data.results, function(n, movie){
 				if(movie.poster_path) {
-					$('#cover-scroller').append('<img src="http://image.tmdb.org/t/p/w185/' + movie.poster_path + '" id="poster-' + n + '" class="poster" alt="image"/>');
+					$('#cover-scroller').append('<div class="poster" style="background-image: url(https://image.tmdb.org/t/p/w300/' + movie.backdrop_path + ')">');
+					//$('#cover-scroller').append('<img src="https://image.tmdb.org/t/p/w185/' + movie.backdrop_path + '" id="poster-' + n + '" class="poster" alt="image"/>');
 				}
 			});
 		});
 })
 
 $('#btn-search').on('click', function (){
-	search_q = encodeURIComponent($('#search').val());
-	console.log(api_url + 'search/movie' + api_key + '&query=' + search_q + '&page=1');
+	var search_q = encodeURIComponent($('#search').val());
 	$.ajax({
-		url: api_url + 'search/movie' + api_key + '&query=' + search_q + '&page=1',
+		url: apiURL('search/movie', 1) + '&query=' + search_q,
 		jsonp: 'callback',
 		dataType: 'jsonp',
 	}).success(function (data){
 		$.each(data.results, function(n, movie) {
 			if(movie.poster_path) {
-				$('.moviecovers').append('<img src="http://image.tmdb.org/t/p/w185/' + movie.poster_path + '" alt="image"/>');
+				$('.moviecovers').append('<img src="https://image.tmdb.org/t/p/w185/' + movie.poster_path + '" alt="image"/>');
 			}
 		});
 	});
